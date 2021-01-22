75a727eaa25766cb60bfc0fbc6c4e62f92d6d1ba
Subhasish-destop <m.subhasish@gmail.com>
2021-01-22 17:56:16 +0530

    sign up with email & password

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,7 +10,33 @@ const config = {
     messagingSenderId: "848562833515",
     appId: "1:848562833515:web:235f9be40dab53e60fc92e",
     measurementId: "G-5BSRHSP4BG"
-  };
+};
+
+export const createUserProfileDocument = async (userAuth,additionalData) => {
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+
+  if(!snapshot.exists){
+    const {displayName , email} =userAuth;
+    const createdAt = new Date();
+
+    try{
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error){
+      console.log('error creating user', error.message);
+
+    }
+  }
+
+  return userRef;
+}
 
 // Initialize Firebase
 firebase.initializeApp(config);
