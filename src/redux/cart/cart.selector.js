b725160bf5206fb1be3726c13b2450fe22fff8d4
Subhasish-destop <m.subhasish@gmail.com>
2021-01-26 12:25:10 +0530

    added checout-item component & page

--- src/redux/cart/cart.selector.js
+++ src/redux/cart/cart.selector.js
@@ -4,14 +4,25 @@ import { createSelector } from 'reselect';
 const selectCart = state => state.cart; 
 
 //output selector - outputs via memoization
-export const selectorCartItems = createSelector(
+export const selectCartItems = createSelector(
   [selectCart],
   cart => cart.cartItems  
 );
 
 //As input you can also use output of other output selector instead of input selector - reuse of memoization/caching
 export const selectCartItemsCount = createSelector(
-    [selectorCartItems],
+    [selectCartItems],
     cartItems =>
         cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,0)
+);
+
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+  );
+  
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems =>
+        cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + (cartItem.price*cartItem.quantity),0)
 );
